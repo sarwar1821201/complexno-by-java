692ae3fec2007bf79d4e86967dc032f500f279e0
Gazi Golam Sarwar <sarwardipo1821201@gmail.com>
2025-03-04 02:46:49 +0600

    create constructor,getter,setter and to string method

--- src/main/java/com/example/complexnooperation/complexNo.java
+++ src/main/java/com/example/complexnooperation/complexNo.java
@@ -4,4 +4,37 @@ public class complexNo {
     int real;
     int img;
 
+// constructor
+    public complexNo(int real, int img) {
+        this.real = real;
+        this.img = img;
+    }
+
+    //getter
+    public int getReal() {
+        return real;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    //setter
+    public void setReal(int real) {
+        this.real = real;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    //for print
+
+    @Override
+    public String toString() {
+        return "complexNo{" +
+                "real=" + real +
+                ", img=" + img +
+                '}';
+    }
 }
