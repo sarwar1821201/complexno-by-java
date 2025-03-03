b1a9c34eaa41b18f3d931edab30d21432733bb34
Gazi Golam Sarwar <sarwardipo1821201@gmail.com>
2025-03-04 03:49:58 +0600

    create model and view opertion

---
+++ src/main/java/com/example/complexnooperation/ComplexOperationViewController.java
@@ -0,0 +1,31 @@
+package com.example.complexnooperation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ComplexOperationViewController {
+
+    @FXML
+    private Button addComplexNoButtonOnAction;
+
+    @FXML
+    private TextField img1stcomplexno;
+
+    @FXML
+    private TextField img2ndcomplexno;
+
+    @FXML
+    private TextField real1stcomplexno;
+
+    @FXML
+    private TextField real2ndcomplexno;
+
+    @FXML
+    private Label resultLabel;
+
+    @FXML
+    private Button subComplexNoButtonOnAction;
+
+}
